7428bd1ed77cfb667af11ca006afbab4daa09555
Nick Klauer <klauer@gmail.com>
2012-10-05 17:01:40 -0500

    describe the project a bit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject java7-watcher.clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Thin wrapper around Java 7's WatchService"
+  :url "https://github.com/klauern/java7-watcher.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
