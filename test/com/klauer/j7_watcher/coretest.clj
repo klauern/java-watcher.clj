b1ac95e72a3393f157fadf84058a47aff7fed3bb
Nick Klauer <klauer@gmail.com>
2012-10-06 14:11:38 -0500

    add some tests

--- test/com/klauer/j7_watcher/coretest.clj
+++ test/com/klauer/j7_watcher/coretest.clj
@@ -1,6 +1,15 @@
-(ns com.klauer.j7-watcher.core
-  (:use [midje.sweet]))
+(ns com.klauer.j7-watcher.coretest
+  (:use [midje.sweet]
+        [com.klauer.j7-watcher.core])
+  (:import [java.nio.file StandardWatchEventKinds]))
 
-(facts "about creating watchers"
-      ())
+(fact "can create watch type from clojure types"
+      (type (make-watch-types-from ["One thing" "Two Things"])) => (class (into-array String []))
+      (let [{:keys [create delete] :as ts} kinds]
+        (class (make-watch-types-from [create delete]) => (class (into-array StandardWatchEventKinds [])))))
 
+
+(fact "can make paths from strings"
+      (instance? java.nio.file.Path (make-path "/usr/local")) => truthy)
+;; (def t (future (make-watch "/Users/klauer/dev/clojure/java7-watcher.clj/watchabledir" [StandardWatchEventKinds/ENTRY_CREATE StandardWatchEventKinds/ENTRY_MODIFY StandardWatchEventKinds/ENTRY_DELETE] #(println "It happened" %))))
+;; (def thing (future (make-watch "/Users/klauer/dev/clojure/java7-watcher.clj/watchabledir" (vals kinds) #(println "event happens" %))))
