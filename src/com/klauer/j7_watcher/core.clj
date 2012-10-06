8170b4ef72654fb4bb780a778cf69cad7fd610b4
Nick Klauer <klauer@gmail.com>
2012-10-06 08:14:26 -0500

    start work on making watch-dir a single function
    As a hack, it's possible to pass one kind of event_type to watch for,
    but it takes four lines of code which are all the same.  I'm working on
    making this simpler or at least one place to set these pieces

--- src/com/klauer/j7_watcher/core.clj
+++ src/com/klauer/j7_watcher/core.clj
@@ -1,14 +1,10 @@
 (ns com.klauer.j7-watcher.core
   (:import [java.nio.file Path Paths StandardWatchEventKinds
-            WatchEvent WatchKey WatchService]))
+            WatchEvent WatchKey WatchService WatchEvent$Kind]))
 
 (def kinds {:create StandardWatchEventKinds/ENTRY_CREATE
             :delete StandardWatchEventKinds/ENTRY_DELETE
             :modify StandardWatchEventKinds/ENTRY_MODIFY})
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
 
 (defn make-path 
   "Creates a java.nio.file.Path object because Paths#get doesn't quite work without stupid fidgity
@@ -19,15 +15,29 @@ stuff like this"
 (defn make-watcher [^Path dir]
   (-> dir .getFileSystem .newWatchService))
 
-(defn register [^WatchService watch watch-kinds directory]
+(defn register-with [^WatchService watch watch-kinds directory]
   (-> directory (.register watch (into-array [watch-kinds]))))
 
-(defn process-events [events]
+(defn- process-events [events]
   (map events #(println %)))
 
-(defn wait-for [watch func]
+(defn wait-for [watch func & args]
   (let [w (.take watch)
         e (.pollEvents w)]    
-    (func)
+    (func args)
+    ;; then we reset the watch key
     (.reset w)
-    (recur watch func)))
+    ;; and start over again
+    (recur watch func args)))
+
+(defn make-watch 
+  "Make a watch on a path 'path', given a seq of kinds (see 'kinds')
+   and passes these events to the `func` with any other arguments `args`" 
+  [path watch-types func & args]
+  (let [p (make-path path)
+        watcher (make-watcher p)]
+    (register-with watcher #^ (into-array [watch-types]) p)
+    (wait-for func args)))
+
+(make-watch "/Users/klauer/dev/clojure/java7-watcher.clj/watchabledir" kinds #(println "event happens"))
+
