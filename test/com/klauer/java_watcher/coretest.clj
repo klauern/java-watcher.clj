9d90703c2f4a3ac08dcf43d587d62e8a92a58dcc
Nick Klauer <klauer@gmail.com>
2012-10-25 06:05:45 -0500

    start testing validation after register/unregister events

--- test/com/klauer/java_watcher/coretest.clj
+++ test/com/klauer/java_watcher/coretest.clj
@@ -1,7 +1,8 @@
 (ns com.klauer.java-watcher.coretest
   (:use [midje.sweet]
-        [com.klauer.java-watcher.core])
-  (:import [java.nio.file StandardWatchEventKinds]))
+        [com.klauer.java-watcher.core]
+        [fs.core :only [temp-dir]])
+  (:import [java.nio.file StandardWatchEventKinds Files]))
 
 (fact "can make paths from strings"
       (instance? java.nio.file.Path (make-path "/usr/local")) => truthy)
@@ -16,6 +17,15 @@
 (future-fact "can register a watch")
 (future-fact "can inspect the registered watches")
 (future-fact "functions are called repeatedly on event changes")
+
+(def dirs [(.toString (temp-dir "one")) (.toString (temp-dir "two"))])
+
+(future-fact "unregistering one watch should NOT invalidate any others"
+      (let [first (register-watch (first dirs) [(:modify kinds)] #(println %))
+            second (register-watch (second dirs) [(:modify kinds)] #(println %))
+            unregistered (unregister-watch (first @registered-watches))]
+        (.isValid (first @registered-watches)) => true))
+
 
 (defn process [event]
   (println "hello event " event))
