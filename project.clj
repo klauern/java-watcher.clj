c5e26a897a4cf98790039cb14329e05c27e64168
Nick Klauer <klauer@gmail.com>
2012-10-25 08:59:15 -0500

    update midje, lein-midje versions

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lamina "0.5.0-beta7"]
-                 [midje "1.4.0" :scope "test"]
+                 [midje "1.5.0-SNAPSHOT" :scope "test"]
                  [fs "1.3.2" :scope "test"]]
   
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]
+  :plugins [[lein-midje "2.0.0"]
             [codox "0.6.1"]])
