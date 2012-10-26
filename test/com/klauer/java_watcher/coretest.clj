b0bd11783ba6607b36e25340b3d796310119d017
Nick Klauer <klauer@gmail.com>
2012-10-25 21:56:10 -0500

    formatting

--- test/com/klauer/java_watcher/coretest.clj
+++ test/com/klauer/java_watcher/coretest.clj
@@ -32,15 +32,15 @@
   (before :facts (reset! registered-watches #{}))
   (after :facts (reset! registered-watches #{})))
 
-  (fact "can register a watch"
-        (let [watch (dummy-watch (first dirs))]
-          (.isValid watch) => true
-          (first @registered-watches) => watch))
-  (fact "registering a watch returns the same watch service in `registered-watches`"
-        (let [watch (dummy-watch (first dirs))]
-          watch => (first @registered-watches)))
-  (fact "unregistering one watch should NOT invalidate any others"
-        (let [first (dummy-watch (first dirs))
-              second (dummy-watch (second dirs))]
-          (unregister-watch first)
-          (.isValid second) => true))
+(fact "can register a watch"
+      (let [watch (dummy-watch (first dirs))]
+        (.isValid watch) => true
+        (first @registered-watches) => watch))
+(fact "registering a watch returns the same watch service in `registered-watches`"
+      (let [watch (dummy-watch (first dirs))]
+        watch => (first @registered-watches)))
+(fact "unregistering one watch should NOT invalidate any others"
+      (let [first (dummy-watch (first dirs))
+            second (dummy-watch (second dirs))]
+        (unregister-watch first)
+        (.isValid second) => true))
