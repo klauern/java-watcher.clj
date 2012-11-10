bdfcb57f4a42ae48aa2d17916b54fbdec814fa82
Nick Klauer <klauer@gmail.com>
2012-11-10 08:50:24 -0600

    more clean up

--- test/com/klauer/file/watcher_test.clj
+++ test/com/klauer/file/watcher_test.clj
@@ -10,20 +10,18 @@
 (defn dummy-register
   ([] (register (.toString (.toPath (fs/temp-dir "tmp"))) #(println "HI") false)))
 
+(fact "dirs registered are stored in a registry"
+      (let [reg (dummy-register)]
+        (count (keys @registry)) => > 1))
+
+(future-fact "directory creation events get registered")
+(future-fact "can register subdirectories recursively")
+
 (fact "can make path from String"
       (instance? java.nio.file.Path (files/make-path (.toString (fs/temp-dir "tmp")))) => truthy)
 (fact "can make path from File"
       (instance? java.nio.file.Path (files/make-path (fs/temp-dir "tmp"))) => truthy)
 
-(fact "dirs registered are stored in a registry"
-             (let [reg (dummy-register)]
-               (count (keys @registry)) => 1)
-             (provided registry => (atom {})))
-(future-fact "directories are registered recursively by default")
-(future-fact "directory creation events get registered")
-(future-fact "can register a directory"
-      (instance? java.nio.file.WatchKey (-> "tmp" fs/temp-dir .toPath .toString register-dir)) => truthy)
-(future-fact "can register subdirectories recursively")(fact "can register a directory with a Path"
 (fact "can register a directory with a Path"
       (instance? java.nio.file.WatchKey (register-dir (-> "tmp" fs/temp-dir .toPath))) => truthy)
 (fact "can register a directory with a Path's String"
