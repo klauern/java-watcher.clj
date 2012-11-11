e096cf6147300945eb1de7d59cc835de5b613694
Nick Klauer <klauer@gmail.com>
2012-11-11 16:23:39 -0600

    more thoughts not committed to code yet

--- test/com/klauer/file/watcher_test.clj
+++ test/com/klauer/file/watcher_test.clj
@@ -17,6 +17,15 @@
 (future-fact "directory creation events get registered")
 (future-fact "can register subdirectories recursively")
 
+;; Need to test some things about actual file-system events causing functions to be called
+;; Midje provides some of that functionality with specifying call-counts
+;; https://github.com/marick/Midje/wiki/Specifying-call-counts
+;; Some things to check for that I have to think about a bit:
+;; - watches continue to watch after multiple events
+;; - multiple watches can act on the same event
+;; -? can unsubscribe watches (not sure about this one, but I suppose it's possible since the registry is separate
+;;    from the blocking watch
+
 (fact "can make path from String"
       (instance? java.nio.file.Path (files/make-path (.toString (fs/temp-dir "tmp")))) => truthy)
 (fact "can make path from File"
