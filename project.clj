0b763a434001e8654ae1240cf828b8ec8c596807
Nick Klauer <klauer@gmail.com>
2013-02-23 20:53:14 -0600

    update dependencies again

--- project.clj
+++ project.clj
@@ -3,14 +3,17 @@
   :url "https://github.com/klauern/java-watcher.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [
+                 [org.clojure/clojure "1.5.0-RC17"]
                  [lamina "0.5.0-beta8" :exclusions [useful]]
                  [potemkin "0.1.6"]
-                 [midje "1.5-alpha6" :scope "test"]
+                 [midje "1.5-beta2" :scope "test"]
                  [fs "1.3.3" :scope "test"]
                  ]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :plugins [[lein-midje "3.0-alpha1"]
-            [codox "0.6.1"]]
+  :plugins [
+            [lein-midje "3.0-beta1"]
+            [codox "0.6.1"]
+            ]
   
   )
