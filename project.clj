48d7789a9f13f0acee5e6d254938edee1eb5ef16
Nick Klauer <klauer@gmail.com>
2013-03-24 13:22:25 -0500

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.klauer/java-watcher "0.1.0-SNAPSHOT"
+(defproject com.klauer/java-watcher "0.1.0"
   :description "Thin wrapper around Java 7's WatchService"
   :url "https://github.com/klauern/java-watcher.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.5.0-RC17"]
+                 [org.clojure/clojure "1.5.1"]
                  [lamina "0.5.0-beta8" :exclusions [useful]]
                  [potemkin "0.1.6"]
                  [midje "1.5-beta2" :scope "test"]
